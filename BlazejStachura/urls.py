e2679e039bef35ed9e964e942787701db8b54125
Mateusz Czaja <mathewczaja@gmail.com>
2022-01-08 22:40:57 +0100

    Beta version of MainPage

--- BlazejStachura/urls.py
+++ BlazejStachura/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from www.views import MainPage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainPage.as_view(), name='Main'),
 ]
--- www/views.py
+++ www/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class MainPage(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'index.html')
+
